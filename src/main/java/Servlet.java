efc57b8c489319c114c5a21485745ca8efe683f9
Krilll-Star <126500722+Krilll-Star@users.noreply.github.com>
2023-04-27 19:36:32 +0300

    Add files via upload

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends HttpServlet {
+    /*
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String id = req.getParameter("id");
+
+        try{
+            writer.println("<h2>Id:" + id + "</h2>");
+        }
+        finally {
+            writer.close();
+        }
+    }
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1> vivod <h1>");
+        out.println("</body></html>");
+    }
+}
