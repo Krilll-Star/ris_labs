d8b5a5f1807eae6b37df8669f230300538f143a6
Krilll-Star <126500722+Krilll-Star@users.noreply.github.com>
2023-09-21 19:46:00 +0300

    Update Ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
+import java.rmi.RemoteException;
 
-public class Ship implements Serializable {
+public class Ship implements ShipBase, Serializable {
 
     private String name;
     private String type;
@@ -10,6 +11,10 @@ public class Ship implements Serializable {
         this.name = name;
         this.type = type;
         this.composition = composition;
+    }
+
+    public Ship() {
+
     }
 
     public String getName() {
@@ -24,7 +29,8 @@ public class Ship implements Serializable {
         return composition;
     }
 
-    public void display() {
+    @Override
+    public void display() throws RemoteException {
         System.out.printf("%s \"%s\" \n", getType(), getName());
         System.out.printf("Состав:\n");
         for (int i = 0; i < getComposition().length; i++) {
